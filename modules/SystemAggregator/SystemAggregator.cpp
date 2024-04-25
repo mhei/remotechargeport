mhei/remotechargeport

commit ca2285656b175446aa375bcb7e70104670cc90ce

SystemAggregator: improve upload of diagnostics
We not do not simply pass down the request, but intercept it
and instruct the systems to upload to us. For this a local
HTTP or FTP server must be running and accepting the uploads.

The URL passed to the systems instead is configurable.

After we received all data from the systems, we finally upload
a combined file to the original URL.

Signed-off-by: Michael Heimpold <mhei@heimpold.de>

modules/SystemAggregator/SystemAggregator.cpp
@@ -1,16 +1,58 @@
 // SPDX-License-Identifier: GPL-3.0-only
 // Copyright Pionix GmbH and Contributors to EVerest
 #include "SystemAggregator.hpp"
+#include <mutex>
 
 namespace module {
 
 void SystemAggregator::init() {
     invoke_init(*p_system);
 
-    // for now, we just pass the first system status
-    this->r_system[0]->subscribe_log_status([this](types::system::LogStatus log_status) {
-        this->p_system->publish_log_status(log_status);
-    });
+    for (std::size_t i = 0; i < this->r_system.size(); ++i) {
+        this->r_system[i]->subscribe_log_status([this, i](types::system::LogStatus log_status) {
+            bool wakeup{false};
+
+            EVLOG_info << (i == 0 ? "Local system" : "Satellite #" + std::to_string(i))
+                       << " reported LogStatus: " << log_status.log_status;
+
+            // Idle and Uploading are not relevant
+            if (log_status.log_status == types::system::LogStatusEnum::Uploading ||
+                log_status.log_status == types::system::LogStatusEnum::Idle)
+                return;
+
+            // all others report status a final feedback (either error or success)
+            // that means that we can awake the waiters
+            // note: it might be possible that we already timed-out and the request_id
+            // is not valid anymore: in this case the following access create a default-constructed
+            // object with is_running = false - we can garbage collected it directly
+            // (other incoming log status updates will do the same)
+
+            // get the lock to access the map etc.
+            std::unique_lock lock(this->lock_log_status);
+
+            this->log_uploads[log_status.request_id].feedback_count++;
+
+            // in case of negative feedback we can drop the expected filename
+            if (log_status.log_status != types::system::LogStatusEnum::Uploaded) {
+                this->log_uploads[log_status.request_id].incoming_filenames[i] = "";
+            }
+
+            // remember whether we need to wakeup anybody
+            wakeup = this->log_uploads[log_status.request_id].is_running;
+
+            // if the upload is not running anymore, we can delete the object
+            if (!this->log_uploads[log_status.request_id].is_running) {
+                this->log_uploads.erase(log_status.request_id);
+            }
+
+            // we should release the lock before notifying
+            lock.unlock();
+
+            if (wakeup) {
+                this->cv_log_status.notify_all();
+            }
+        });
+    }
 
     this->r_system[0]->subscribe_firmware_update_status([this](types::system::FirmwareUpdateStatus firmware_update_status) {
         this->p_system->publish_firmware_update_status(firmware_update_status);

modules/SystemAggregator/SystemAggregator.hpp
@@ -14,29 +14,53 @@
 #include <generated/interfaces/system/Implementation.hpp>
 
 // headers for required interface implementations
+#include <generated/interfaces/satellite/Interface.hpp>
 #include <generated/interfaces/system/Interface.hpp>
 
 // ev@4bf81b14-a215-475c-a1d3-0a484ae48918:v1
 // insert your custom include headers here
+#include <condition_variable>
+#include <cstdint>
+#include <map>
+#include <mutex>
+#include "systemaggregator_upload_log_request.hpp"
 // ev@4bf81b14-a215-475c-a1d3-0a484ae48918:v1
 
 namespace module {
 
-struct Conf {};
+struct Conf {
+    std::string upload_url_template;
+};
 
 class SystemAggregator : public Everest::ModuleBase {
 public:
     SystemAggregator() = delete;
     SystemAggregator(const ModuleInfo& info, std::unique_ptr<systemImplBase> p_system,
-                     std::vector<std::unique_ptr<systemIntf>> r_system, Conf& config) :
-        ModuleBase(info), p_system(std::move(p_system)), r_system(std::move(r_system)), config(config){};
+                     std::vector<std::unique_ptr<systemIntf>> r_system,
+                     std::vector<std::unique_ptr<satelliteIntf>> r_satellite, Conf& config) :
+        ModuleBase(info),
+        p_system(std::move(p_system)),
+        r_system(std::move(r_system)),
+        r_satellite(std::move(r_satellite)),
+        config(config){};
 
     const std::unique_ptr<systemImplBase> p_system;
     const std::vector<std::unique_ptr<systemIntf>> r_system;
+    const std::vector<std::unique_ptr<satelliteIntf>> r_satellite;
     const Conf& config;
 
     // ev@1fce4c5e-0ab8-41bb-90f7-14277703d2ac:v1
     // insert your public definitions here
+
+    // maps for all ongoing log upload requests
+    std::map<int32_t, systemaggregator_upload_log_request> log_uploads;
+    std::map<std::string, int32_t> type_to_log_uploads_map;
+
+    // condition variable signaling updated log status
+    std::condition_variable cv_log_status;
+
+    // mutex to protect the maps and cv
+    std::mutex lock_log_status;
     // ev@1fce4c5e-0ab8-41bb-90f7-14277703d2ac:v1
 
 protected:

modules/SystemAggregator/manifest.yaml
@@ -1,5 +1,15 @@
 description: >-
   Aggregates multiple system interface into a single one.
+config:
+  upload_url_template:
+    description: >-
+      The URL template for the satellites to upload to when uploading diagnostics.
+      The string must contain <my-ip> which is replaced with the actual IP address
+      of the system the SystemAggregator is running on.
+      Usually, the URL should end with a trailing slash to indicate that it is a
+      directory where to place the uploaded file.
+    type: string
+    default: "ftp://{my-ip}:2121/"
 provides:
   system:
     interface: system
@@ -9,6 +19,10 @@ requires:
     interface: system
     min_connections: 2
     max_connections: 4
+  satellite:
+    interface: satellite
+    min_connections: 1
+    max_connections: 3
 metadata:
   license: https://opensource.org/license/gpl-3-0
   authors:

commit 1280bc18ebfbdf6c762c612b953d369b539f74f4

SystemAggregator: add missing changed chunks of last commit
The previous commit was incomplete and missed important changes.

Fixes: ca2285656b17 ("SystemAggregator: improve upload of diagnostics")
Signed-off-by: Michael Heimpold <michael.heimpold@chargebyte.com>

modules/SystemAggregator/CMakeLists.txt
@@ -18,4 +18,10 @@ target_sources(${MODULE_NAME}
 
 # ev@c55432ab-152c-45a9-9d2e-7281d50c69c3:v1
 # insert other things like install cmds etc here
+
+install(
+    PROGRAMS
+        logs_upload_helper.sh
+    DESTINATION "${EVEREST_MODULE_INSTALL_PREFIX}/${MODULE_NAME}"
+)
 # ev@c55432ab-152c-45a9-9d2e-7281d50c69c3:v1

modules/SystemAggregator/SystemAggregator.cpp
@@ -12,8 +12,8 @@ void SystemAggregator::init() {
         this->r_system[i]->subscribe_log_status([this, i](types::system::LogStatus log_status) {
             bool wakeup{false};
 
-            EVLOG_info << (i == 0 ? "Local system" : "Satellite #" + std::to_string(i))
-                       << " reported LogStatus: " << log_status.log_status;
+            EVLOG_info << "System #" << i << " reported LogStatus: " << log_status.log_status
+                       << " (" << log_status.request_id << ")";
 
             // Idle and Uploading are not relevant
             if (log_status.log_status == types::system::LogStatusEnum::Uploading ||
@@ -34,6 +34,7 @@ void SystemAggregator::init() {
 
             // in case of negative feedback we can drop the expected filename
             if (log_status.log_status != types::system::LogStatusEnum::Uploaded) {
+                EVLOG_debug << "System #" << i << ": dropping filename due to reported error.";
                 this->log_uploads[log_status.request_id].incoming_filenames[i] = "";
             }
 
@@ -42,6 +43,7 @@ void SystemAggregator::init() {
 
             // if the upload is not running anymore, we can delete the object
             if (!this->log_uploads[log_status.request_id].is_running) {
+                EVLOG_debug << "System #" << i << ": not running anymore, cleaning up.";
                 this->log_uploads.erase(log_status.request_id);
             }
 

modules/SystemAggregator/SystemAggregator.hpp
@@ -19,17 +19,21 @@
 
 // ev@4bf81b14-a215-475c-a1d3-0a484ae48918:v1
 // insert your custom include headers here
+#include "systemaggregator_upload_log_request.hpp"
 #include <condition_variable>
 #include <cstdint>
 #include <map>
 #include <mutex>
-#include "systemaggregator_upload_log_request.hpp"
 // ev@4bf81b14-a215-475c-a1d3-0a484ae48918:v1
 
 namespace module {
 
 struct Conf {
+    int default_retries;
+    int default_retry_interval;
     std::string upload_url_template;
+    std::string incoming_uploads_dir;
+    int incoming_upload_timeout;
 };
 
 class SystemAggregator : public Everest::ModuleBase {

modules/SystemAggregator/logs_upload_helper.sh
@@ -1,0 +1,52 @@
+#!/bin/sh
+#
+# This helper received the following arguments:
+# - the filename to use for the upload
+# - the target URL to upload to
+# - the file names to store into the tarball
+#   (relative paths, i.e. only the filenames and
+#   assumed to be placed inside UPLOAD_DIR)
+#
+
+DEFAULTS_FILE="/etc/default/systemaggregatorftpd"
+UPLOAD_DIR="/srv/diagnostics-incoming"
+WORK_DIR="/srv/diagnostics-outgoing"
+CONNECTION_TIMEOUT="20"
+
+FN="$1"
+URL="$2"
+shift 2
+
+[ -f "$DEFAULTS_FILE" ] && . "$DEFAULTS_FILE"
+
+# just in case it does not exist yet
+mkdir -p "$WORK_DIR"
+
+# tar the given files into a single archive
+if ! tar -czf "$WORK_DIR/$FN" -C "$UPLOAD_DIR" "$@"; then
+    # error, so cleanup and report it
+    cd "$UPLOAD_DIR"
+    rm -f "$@"
+    echo "UploadFailure"
+    exit 1
+fi
+
+curl --progress-bar --connect-timeout "$CONNECTION_TIMEOUT" -T "$WORK_DIR/$FN" "$URL"
+curl_exit_code=$?
+if [[ $curl_exit_code -eq 0 ]]; then
+    echo "Uploaded"
+elif [[ $curl_exit_code -eq 67 ]] || [[ $curl_exit_code -eq 35 ]] || [[ $curl_exit_code -eq 69 ]] ||
+    [[ $curl_exit_code -eq 9 ]]; then
+    echo "PermissionDenied"
+elif [[ $curl_exit_code -eq 3 ]] || [[ $curl_exit_code -eq 6 ]] || [[ $curl_exit_code -eq 10 ]] ||
+    [[ $curl_exit_code -eq 87 ]]; then
+    echo "BadMessage"
+elif [[ $curl_exit_code -eq 1 ]]; then
+    echo "NotSupportedOperation"
+else
+    echo "UploadFailure"
+fi
+
+# final cleanup: only cleanup the tarball,
+# the incoming uploads might be required for a retry, so keep them
+rm "$WORK_DIR/$FN"

modules/SystemAggregator/manifest.yaml
@@ -1,6 +1,18 @@
 description: >-
   Aggregates multiple system interface into a single one.
 config:
+  default_retries:
+    description: Specifies how many times the Charge Point retries to upload or download files on previous failure.
+    type: integer
+    minimum: 0
+    maximum: 5
+    default: 0
+  default_retry_interval:
+    description: Specifies in seconds after which time a retry of an upload or download on previous failure may be attempted.
+    type: integer
+    minimum: 0
+    maximum: 3600
+    default: 180
   upload_url_template:
     description: >-
       The URL template for the satellites to upload to when uploading diagnostics.
@@ -10,6 +22,16 @@ config:
       directory where to place the uploaded file.
     type: string
     default: "ftp://{my-ip}:2121/"
+  incoming_uploads_dir:
+    description: The directory where incoming uploads are expected.
+    type: string
+    default: /srv/diagnostics-incoming
+  incoming_upload_timeout:
+    description: Specifies in seconds, how long we wait at max for incoming uploads.
+    type: integer
+    minimum: 30
+    maximum: 600
+    default: 120
 provides:
   system:
     interface: system

modules/SystemAggregator/system/systemImpl.cpp
@@ -7,11 +7,13 @@
 #include <condition_variable>
 #include <cstdint>
 #include <limits>
+#include <filesystem>
 #include <regex>
 #include <random>
 #include <thread>
 #include <vector>
-#include <utils/date.hpp>
+#include <fmt/chrono.h>
+#include <boost/process.hpp>
 #include "../systemaggregator_upload_log_request.hpp"
 
 using namespace std::chrono_literals;
@@ -52,10 +54,25 @@ int32_t systemImpl::create_random_request_id() {
     return rand_int32(rand_engine);
 }
 
+std::string systemImpl::create_logs_filename(std::string type) {
+    using namespace std::chrono;
+    time_point<system_clock> now{system_clock::now()};
+
+    // note: seems that different versions of libfmt give different results, e.g.
+    // sometimes format %S as decimal number, sometimes as integer;
+    // workaround: cut possible trailing suffix .1234 away
+    std::string ts = fmt::format("{:%FT%H-%M-%S}", now);
+
+    if (ts.find(".") != std::string::npos)
+       ts = ts.substr(0, ts.find("."));
+
+    return type + "_" + ts + ".tar.gz";
+}
+
 types::system::UploadLogsResponse
 systemImpl::handle_upload_logs(types::system::UploadLogsRequest& upload_logs_request) {
     std::scoped_lock lock(this->mod->lock_log_status);
-    std::chrono::duration upload_timeout{3min};
+    std::chrono::seconds upload_timeout{this->mod->config.incoming_upload_timeout};
     int32_t request_id;
 
     // for now we assume that the request_id namespace is unique, i.e.
@@ -64,7 +81,7 @@ systemImpl::handle_upload_logs(types::system::UploadLogsRequest& upload_logs_req
     // default empty string for request with unspecified type (aka OCPP 1.6)
     std::string type = upload_logs_request.type.value_or("{undefined}");
 
-    EVLOG_info << "Got upload request of type \"" << type << "\".";
+    EVLOG_info << "Got log upload request of type \"" << type << "\".";
 
     // either we already have a log upload for a given type, then we should find it here
     if (auto s = this->mod->type_to_log_uploads_map.find(type); s != this->mod->type_to_log_uploads_map.end()) {
@@ -81,14 +98,16 @@ systemImpl::handle_upload_logs(types::system::UploadLogsRequest& upload_logs_req
     } else {
         // or we have to create a new one, but in this case we use the request_id (or generate one)
         request_id = upload_logs_request.request_id.value_or(this->create_random_request_id());
+        std::string fn = this->create_logs_filename(upload_logs_request.type.value_or("diagnostics"));
 
         this->mod->log_uploads[request_id].request_id = request_id;
         this->mod->log_uploads[request_id].type = type;
-        this->mod->log_uploads[request_id].filename = "fixme-upload.tar.gz";
+        this->mod->log_uploads[request_id].filename = fn;
         this->mod->log_uploads[request_id].is_running = true;
-        this->mod->log_uploads[request_id].orig_request = upload_logs_request;
 
         this->mod->type_to_log_uploads_map[type] = request_id;
+
+        EVLOG_info << "This is now handled with internal request id " << request_id << " and will provide '" << fn << "'.";
     }
 
     // we tweak the request a little bit:
@@ -104,15 +123,17 @@ systemImpl::handle_upload_logs(types::system::UploadLogsRequest& upload_logs_req
     modified_request.request_id.emplace(request_id);
 
     // push request out to local system and satellites
-    for (std::size_t i = 0; i < this->mod->r_system.size(); ++i) {
+    // note: i wraps so the comparision is special
+    for (std::size_t i = this->mod->r_system.size() - 1; i < this->mod->r_system.size(); --i) {
+
         modified_request.location =
             std::regex_replace(this->mod->config.upload_url_template,
                                std::regex("{my-ip}", std::regex::basic|std::regex::icase),
                                i == 0
                                    ? "127.0.0.1"
-                                   : this->mod->r_satellite[i]->call_get_local_endpoint_address());
+                                   : this->mod->r_satellite[i - 1]->call_get_local_endpoint_address());
 
-        EVLOG_info << "Requesting diagnostics upload from system #" << i << ".";
+        EVLOG_info << "Requesting logs upload from system #" << i << ".";
 
         // examine collected return values; in at least one is unhappy mark maybe
         // running uploads as to cancel - but we don't pass down cancellation, though
@@ -130,21 +151,36 @@ systemImpl::handle_upload_logs(types::system::UploadLogsRequest& upload_logs_req
         }
 
         // remember the filename (if given) we have to handle later
-        this->mod->log_uploads[request_id].incoming_filenames[i] = status.file_name.value_or("");
+        // note: for security reason we pass it through std::filesystem to extract the real filename
+        // and cut off possible paths before and we check for special values which are removed if present
+        std::filesystem::path fn{status.file_name.value_or("")};
+        std::string fn_filtered = fn.filename().string();
+        if (fn_filtered == "." || fn_filtered == "..")
+           fn_filtered = "";
+        this->mod->log_uploads[request_id].incoming_filenames[i] = fn_filtered;
+        if (fn_filtered != fn.string()) {
+            EVLOG_warning << "System #" << i << " tried to use suspicious filename (" << fn << "), filtered.";
+        }
 
         EVLOG_info << "System #" << i << " will upload '" << this->mod->log_uploads[request_id].incoming_filenames[i] << "'.";
     }
 
-    EVLOG_info << "Local system and satellites instructed, proceeding.";
+    EVLOG_info << "All systems instructed, all fine, proceeding.";
 
     // at this point the local system and all satellites are informed and
     // accepted the upload request, we wait for incoming log status updates
-    std::thread([this, type, upload_timeout, request_id]() {
+    std::thread([this, type, upload_timeout, request_id, upload_logs_request]() {
         std::unique_lock<std::mutex> lock(this->mod->lock_log_status);
+        std::filesystem::path incoming_basedir{this->mod->config.incoming_uploads_dir};
+
+        bool upload_completed{false};
+        std::chrono::seconds retry_interval{this->mod->config.default_retry_interval};
+        unsigned int max_retries{static_cast<unsigned int>(upload_logs_request.retries.value_or(this->mod->config.default_retries))};
+        unsigned int retries{0};
 
         // we just inform the backend that we are "about to start" uploading to prevent backend timeouts
         types::system::LogStatus reported_status{types::system::LogStatusEnum::Uploading,
-                                                 this->mod->log_uploads[request_id].orig_request.request_id.value_or(0)};
+                                                 upload_logs_request.request_id.value_or(0)};
         this->publish_log_status(reported_status);
 
         EVLOG_info << "Waiting for incoming status messages...";
@@ -152,7 +188,7 @@ systemImpl::handle_upload_logs(types::system::UploadLogsRequest& upload_logs_req
         if (!this->mod->cv_log_status.wait_for(lock,
                                                upload_timeout,
                                                [this, request_id]{
-                                                   bool got_feedback_from_all{this->mod->log_uploads[request_id].feedback_count == this->mod->r_satellite.size()};
+                                                   bool got_feedback_from_all{this->mod->log_uploads[request_id].feedback_count == this->mod->r_system.size()};
                                                    bool not_running_anymore = !this->mod->log_uploads[request_id].is_running;
                                                    // we don't need to wait any longer if...
                                                    return got_feedback_from_all or not_running_anymore;
@@ -169,20 +205,70 @@ systemImpl::handle_upload_logs(types::system::UploadLogsRequest& upload_logs_req
             EVLOG_info << "All systems uploaded, proceeding.";
         }
 
-        std::this_thread::sleep_for(5s);
+        // create cmdline parameters for our helper
+        std::vector<std::string> args;
+        args.push_back(this->mod->log_uploads[request_id].filename);
+        args.push_back(upload_logs_request.location);
+        for (auto& it : this->mod->log_uploads[request_id].incoming_filenames) {
+            // if filename is not empty, include it in the parameters
+            if (it.second != "")
+                args.push_back(it.second);
+        }
 
-        // finally report success
-        reported_status.log_status = types::system::LogStatusEnum::Uploaded;
-        this->publish_log_status(reported_status);
+        while (!upload_completed &&
+               retries <= max_retries &&
+               this->mod->log_uploads[request_id].is_running) {
+            std::filesystem::path libexec_dir{this->mod->info.paths.libexec};
+            auto fn_helper{libexec_dir / "logs_upload_helper.sh"};
+            std::string line;
+            retries++;
 
-        EVLOG_info << "Upload of type \"" << type << "\" completed.";
+            boost::process::ipstream stream;
+            boost::process::child helper(fn_helper.string(), boost::process::args(args), boost::process::std_out > stream);
+
+            while (std::getline(stream, line) && this->mod->log_uploads[request_id].is_running) {
+                if (line == "Uploaded") {
+                    reported_status.log_status = types::system::string_to_log_status_enum(line);
+                    this->publish_log_status(reported_status);
+                } else if (line == "UploadFailure" ||
+                           line == "PermissionDenied" ||
+                           line == "BadMessage" ||
+                           line == "NotSupportedOperation") {
+                    reported_status.log_status = types::system::LogStatusEnum::UploadFailure;
+                    this->publish_log_status(reported_status);
+                }
+                EVLOG_debug << "Upload helper said: " << line;
+            }
+
+            if (!this->mod->log_uploads[request_id].is_running) {
+                EVLOG_info << "While processing, request to cancel upload of type \"" << type << "\" received.";
+                helper.terminate();
+            } else if (reported_status.log_status != types::system::LogStatusEnum::Uploaded &&
+                       retries <= max_retries) {
+                std::this_thread::sleep_for(retry_interval);
+            } else {
+                upload_completed = true;
+            }
+            helper.wait();
+        }
+
+        // cleanup the incoming files, our own generated tarball was already handled
+        for (auto& it : this->mod->log_uploads[request_id].incoming_filenames) {
+            // if filename is not empty, include it in the parameters
+            if (it.second != "") {
+                auto abs_fn{incoming_basedir / it.second};
+
+                EVLOG_debug << "Removing file " << abs_fn;
+                std::filesystem::remove(abs_fn);
+            }
+        }
+
+        EVLOG_info << "Upload of type \"" << type << "\" finally processed.";
 
         // cleanup: first delete the map with the pointer, then the object pointed to
         this->mod->type_to_log_uploads_map.erase(type);
         this->mod->log_uploads.erase(request_id);
 	}).detach();
-
-    EVLOG_info << "Thread launched.";
 
     return {types::system::UploadLogsStatus::Accepted, this->mod->log_uploads[request_id].filename};
 }

modules/SystemAggregator/system/systemImpl.hpp
@@ -47,7 +47,11 @@ protected:
     // ev@d2d1847a-7b88-41dd-ad07-92785f06f5c4:v1
     // insert your protected definitions here
 
+    // create a random request id for internal use
     int32_t create_random_request_id();
+
+    // create a reasonable filename for the file to upload
+    std::string create_logs_filename(std::string type);
     // ev@d2d1847a-7b88-41dd-ad07-92785f06f5c4:v1
 
 private:

modules/SystemAggregator/systemaggregator_upload_log_request.hpp
@@ -3,9 +3,10 @@
 
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
+#include <map>
 #include <string>
-#include <vector>
 #include <generated/interfaces/system/Implementation.hpp>
 #include <nlohmann/json.hpp>
 
@@ -19,33 +20,5 @@ struct systemaggregator_upload_log_request {
     bool is_running; ///< Whether the upload is still ongoing
     unsigned int feedback_count; ///< Count of (final) feedbacks received
 
-    std::vector<std::string> incoming_filenames; ///< List of filenames we will receive
-
-    types::system::UploadLogsRequest orig_request; ///< Copy of the original request
-#if 0
-    friend void to_json(json& j, const systemaggregator_upload_log_request& k) {
-        j = json{{"request_id", k.request_id},
-                 {"type", k.type},
-                 {"filename", k.filename},
-                 {"is_running", k.is_running},
-                 {"is_cancelled", k.is_cancelled}};
-        j["feedback_count"] = k.feedback_count;
-        j["orig_request"] = k.orig_request;
-    }
-
-    friend void from_json(const json& j, systemaggregator_upload_log_request& k) {
-        k.request_id = j.at("request_id");
-        k.type = j.at("type");
-        k.filename = j.at("filename");
-        k.is_running = j.at("is_running");
-        k.is_cancelled = j.at("is_cancelled");
-        k.feedback_count = j.at("feedback_count");
-        k.orig_request = j.at("orig_request");
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const systemaggregator_upload_log_request& k) {
-        os << json(k).dump(4);
-        return os;
-    }
-#endif
+    std::map<std::size_t, std::string> incoming_filenames; ///< Map of filenames we will receive
 };