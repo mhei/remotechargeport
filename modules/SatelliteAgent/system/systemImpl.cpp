2fbb0d1b6ba5780cb1e16122922782700738cafe
Michael Heimpold <mhei@heimpold.de>
2025-03-22 17:10:03 +0100

    SatelliteAgent: silence compiler warnings of unused interface handlers
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>

--- modules/SatelliteAgent/system/systemImpl.cpp
+++ modules/SatelliteAgent/system/systemImpl.cpp
@@ -14,6 +14,7 @@ void systemImpl::ready() {
 
 types::system::UpdateFirmwareResponse
 systemImpl::handle_update_firmware(types::system::FirmwareUpdateRequest& firmware_update_request) {
+    (void)firmware_update_request;
     return types::system::UpdateFirmwareResponse::Rejected;
 }
 
@@ -22,17 +23,22 @@ void systemImpl::handle_allow_firmware_installation() {
 
 types::system::UploadLogsResponse
 systemImpl::handle_upload_logs(types::system::UploadLogsRequest& upload_logs_request) {
+    (void)upload_logs_request;
     return {};
 }
 
 bool systemImpl::handle_is_reset_allowed(types::system::ResetType& type) {
+    (void)type;
     return false;
 }
 
 void systemImpl::handle_reset(types::system::ResetType& type, bool& scheduled) {
+    (void)type;
+    (void)scheduled;
 }
 
 bool systemImpl::handle_set_system_time(std::string& timestamp) {
+    (void)timestamp;
     return false;
 }
 
